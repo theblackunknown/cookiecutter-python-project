b11491d2cbe31457940780adaca71300bdb2b670
MACHIZAUD Andréa <andrea.machizaud@gmail.com>
2019-11-24 09:17:14 +0100

    escape folder path in script to accept space

--- {{ cookiecutter.project_name }}/build-packages.cmd
+++ {{ cookiecutter.project_name }}/build-packages.cmd
@@ -1,7 +1,7 @@
 
 setlocal
 
-@for %%G IN (%~dp0modules\module-{{ cookiecutter.project_name }} %~dp0applications\application-{{ cookiecutter.project_name }}) do (
+@for %%G IN ("%~dp0modules\module-{{ cookiecutter.project_name }}" "%~dp0applications\application-{{ cookiecutter.project_name }}") do (
     pushd %%G
     call py setup.py sdist bdist_wheel
     @popd
--- {{ cookiecutter.project_name }}/remove-built-packages.cmd
+++ {{ cookiecutter.project_name }}/remove-built-packages.cmd
@@ -3,7 +3,7 @@ setlocal
 
 rem Add git commit hash to built packages
 
-@for %%G IN (%~dp0modules\module-{{ cookiecutter.project_name }} %~dp0applications\application-{{ cookiecutter.project_name }}) do (
+@for %%G IN ("%~dp0modules\module-{{ cookiecutter.project_name }}" "%~dp0applications\application-{{ cookiecutter.project_name }}") do (
     pushd %%G
     rmdir /S /Q dist build 2>NUL
     @popd
